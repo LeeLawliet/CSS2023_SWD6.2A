044e7f0df19ba2e564b0426ca28f947528013c96
DESKTOP-5V3R713\User <lee.xerri.e21932@mcast.edu.mt>
2024-01-02 00:53:46 +0100

    Add droneregistration to git

--- product-mgmt/src/app/app.component.ts
+++ product-mgmt/src/app/app.component.ts
@@ -6,7 +6,6 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  
   pageTitle: string = "This is a Product Management System";
 
 }
